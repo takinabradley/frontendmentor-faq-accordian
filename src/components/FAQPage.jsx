5c21e58a0865a313e2130ec7c345b553482d5e78
Bradley Finney <77649883+takinabradley@users.noreply.github.com>
2024-06-03 23:12:21 -0400

    open first detail when rendered

--- src/components/FAQPage.jsx
+++ src/components/FAQPage.jsx
@@ -1,7 +1,10 @@
 import H from "./H"
 import styles from "./FAQPage.module.css"
+import { useEffect, useRef } from "react";
 
 export default function FAQPage({FAQs = [], headingLevelStart = 1, children}) {
+  const ref = useRef(null)
+
   const onClick = e => {
     e.preventDefault();
     if (e.currentTarget.open) {
@@ -10,8 +13,6 @@ export default function FAQPage({FAQs = [], headingLevelStart = 1, children}) {
       [...e.currentTarget.parentElement.children].forEach(elem => elem.removeAttribute('open'))
       e.currentTarget.setAttribute('open', '')
     }
-
-    
   }
 
   return (
@@ -22,9 +23,9 @@ export default function FAQPage({FAQs = [], headingLevelStart = 1, children}) {
         {children}
       </div>
       
-      <div className={styles["faqs"]} >
-        {FAQs.map(FAQ => 
-          <details key={FAQ.question} name='FAQ' className={styles["faq"]} onClick={onClick}>
+      <div className={styles["faqs"]} ref={ref}>
+        {FAQs.map((FAQ, index) => 
+          <details key={FAQ.question} name='FAQ' className={styles["faq"]} onClick={onClick} open={index === 0 ? true : false}>
             <summary className={styles["faqQuestion"]}>
               <H level={headingLevelStart + 1} className={styles["faqQuestionHeading"]}>
                 {FAQ.question}
